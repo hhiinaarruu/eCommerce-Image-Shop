e3bfc0215bfd87f6a359146fd667bbb564805352
paul <hhiinaarruu@gmail.com>
2019-01-09 16:53:17 +0700

    2.5 Searching feature finish

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :charges, only: [:new, :create]
   resources :line_items
   resources :carts
-  resources :pictures
+  resources :pictures do
+    collection do
+      get :search
+    end
+  end
 
   devise_for :users, controllers: {
     registrations: 'registrations'
